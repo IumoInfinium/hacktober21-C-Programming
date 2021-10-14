e7364a308a7836337f8a8b88cf623d2d499d4d2d
Naman-sharma00100 <84118525+Naman-sharma00100@users.noreply.github.com>
2021-10-14 15:30:36 +0530

    Create BubbleSort_usingFunctions.c
    This is the file for bubble sort with user input using functions.

---
+++ BubbleSort_usingFunctions.c
@@ -0,0 +1,48 @@
+// C program for implementation of Bubble sort
+#include <stdio.h>
+ 
+void swap(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+ 
+// A function to implement bubble sort
+void bubbleSort(int arr[], int n)
+{
+   int i, j;
+   for (i = 0; i < n-1; i++)     
+ 
+       // Last i elements are already in place  
+       for (j = 0; j < n-i-1; j++)
+           if (arr[j] > arr[j+1])
+              swap(&arr[j], &arr[j+1]);
+}
+ 
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+ 
+// Driver program to test above functions
+int main()
+{
+    int arr[100],no;
+    printf("Enter the no of elements\n");
+    scanf("%d",&no);
+    printf("Enter the elements\n");
+    for(int i=0;i<no;i++)
+    {
+      scanf("%d",&arr[i]);
+    }
+    int n = sizeof(arr)/sizeof(arr[0]);
+    bubbleSort(arr, n);
+    printf("Sorted array: \n");
+    printArray(arr, n);
+    return 0;
+}
